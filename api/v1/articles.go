e61e64392b81fd33efcd13e3cbbb544c065881d4
Dmitrii Rubtsov <dm.mironov01@gmail.com>
2024-08-03 15:31:55 +0300

    WIP: request params

--- api/v1/articles.go
+++ api/v1/articles.go
@@ -1,10 +1,13 @@
 package v1
 
 import (
+	"fmt"
+	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/render"
 	"github.com/massivemadness/articles-server/api/common"
 	"github.com/massivemadness/articles-server/internal/articles"
 	"net/http"
+	"strconv"
 )
 
 func GetArticlesHandler(asv articles.ArticleService) http.HandlerFunc {
@@ -18,6 +21,13 @@ func GetArticlesHandler(asv articles.ArticleService) http.HandlerFunc {
 
 func GetArticleHandler(_ articles.ArticleService) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		reqID := chi.URLParam(r, "id")
+		artID, err := strconv.ParseInt(reqID, 10, 32)
+		if err != nil {
+			fmt.Printf("conversion error: %e\n", err)
+		}
+		fmt.Printf("artID = %d\n", artID)
+
 		render.Status(r, http.StatusBadRequest)
 		render.JSON(w, r, common.HttpError{
 			ErrorMessage: "Not implemented",
